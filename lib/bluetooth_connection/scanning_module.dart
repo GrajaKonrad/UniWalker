61b3a5c068bef1254026ea4bc786d5a306bd09c8
inf148124 <konrad.graja@gmail.com>
2023-10-16 18:26:59 +0200

    BT scanner implementation

--- lib/bluetooth_connection/scanning_module.dart
+++ lib/bluetooth_connection/scanning_module.dart
@@ -1,2 +1,53 @@
-import 'package:flutter/material.dart';
-impo
+import 'dart:async';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'dart:io' show Platform;
+
+class BluetoothScanner {
+  const BluetoothScanner();
+
+  Future startBluetooth() async
+  {
+    FlutterBluePlus.setLogLevel(LogLevel.verbose, color:false);
+    print("DONE2");
+
+    if (await FlutterBluePlus.isSupported == false) {
+      print("Bluetooth not supported by this device");
+      return;
+    }
+
+    FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) {
+      print(state);
+      if (state == BluetoothAdapterState.on) {
+        _scanBluetooth();
+      } else {
+        unawaited(FlutterBluePlus.stopScan());
+        print("Bluetooth Stopped");
+      }
+    });
+
+    if (Platform.isAndroid) {
+      await FlutterBluePlus.turnOn();
+    }
+  }
+
+  Future _scanBluetooth() async
+  {
+    // Setup Listener for scan results.
+    // device not found? see "Common Problems" in the README
+    Set<DeviceIdentifier> seen = {};
+    var subscription = FlutterBluePlus.scanResults.listen(
+            (results) {
+              for (ScanResult r in results) {
+                if (seen.contains(r.device.remoteId) == false) {
+                  print('${r.device.remoteId}: "${r.advertisementData.localName}" found! rssi: ${r.rssi}');
+                  //seen.add(r.device.remoteId);
+                }
+              }
+            },
+          onError: (e) => print(e)
+      );
+
+    // Start scanning
+    await FlutterBluePlus.startScan();
+  }
+}
